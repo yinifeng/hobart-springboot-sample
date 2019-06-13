190fec78fa3310ab7c7ac372c50113e6f07cc1c5
yinifeng <1396997785@qq.com>
2019-06-14 00:53:57 +0800

    整合通用mapper

--- hobart-sample-server/src/main/java/com/hobart/sample/HobartApplication.java
+++ hobart-sample-server/src/main/java/com/hobart/sample/HobartApplication.java
@@ -5,8 +5,10 @@ import com.hobart.sample.core.CoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.hobart.sample.mapper")
 //@ComponentScan(basePackageClasses={CoreConfig.class, CommonConfig.class})
 public class HobartApplication {
 
