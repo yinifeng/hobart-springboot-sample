df8195b2817d0e7ae768e519d4b7b3f69e1fa2ef
yinifeng <1396997785@qq.com>
2019-06-24 20:11:08 +0800

    整合hibernate

---
+++ hobart-sample-hibernate/src/main/java/com.hobart.sample.hibernate/HobartHibernateApplication.java
@@ -0,0 +1,11 @@
+package com.hobart.sample.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobartHibernateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HobartHibernateApplication.class,args);
+    }
+}
